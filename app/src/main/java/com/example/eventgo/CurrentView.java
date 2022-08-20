a33959991e8a4d251e26fe706ca008c2b7d19b45
bithi26 <abulkalamazad13520@gmail.com>
2022-08-21 01:59:52 +0600

    current event

---
+++ app/src/main/java/com/example/eventgo/CurrentView.java
@@ -0,0 +1,14 @@
+package com.example.eventgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_view);
+    }
+}
--- app/src/main/java/com/example/eventgo/second_page.java
+++ app/src/main/java/com/example/eventgo/second_page.java
@@ -8,23 +8,39 @@ import android.view.View;
 import android.widget.Button;
 
 public class second_page extends AppCompatActivity {
-    private Button button;
+    private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_page);
-        button =(Button) findViewById(R.id.createbutton);
-        button.setOnClickListener(new View.OnClickListener() {
+        button1 =(Button) findViewById(R.id.createbutton);
+        button2=(Button)findViewById(R.id.checkcurbutton);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openthirdpage();
             }
+        });
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View view)
+            {
+                openCurrentEvent();
+            }
+
         });
     }
     public void openthirdpage(){
         Intent intent = new Intent(this, third_page.class);
         startActivity(intent);
     }
+    public void openCurrentEvent()
+    {
+        Intent intent=new Intent(this,CurrentEvent.class);
+        startActivity(intent);
+    }
+
 
 }
