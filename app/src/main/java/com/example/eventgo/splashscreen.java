fe4a56a7ffce93f2327dfa6f2975fa1b9484d02b
fardin-pranto43 <2019-417-826@student.cse.du.ac.bd>
2022-11-14 21:54:21 +0600

    "animation"

--- app/src/main/java/com/example/eventgo/splashscreen.java
+++ app/src/main/java/com/example/eventgo/splashscreen.java
@@ -5,41 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.example.eventgo.R;
 
 public class splashscreen extends AppCompatActivity {
+
+    private  static  int SPLASH_SCREEN =4500;
+
+    ImageView imageView;
+    TextView textView1, textView2;
+    Animation top, bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreen);
 
-        Thread thread = new Thread(){
-            public void run()
-            {
-                try{
-                    sleep(3000);
-                }
-                catch (Exception e){
-                    e.printStackTrace();
+        imageView = findViewById(R.id.imageView);
+        textView1 = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
 
-                }
-                finally {
-                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                    if (user != null) {
-                        startActivity(new Intent(splashscreen.this,second_page.class));
 
-                    }
-                    else {
-                        Intent intent = new Intent(splashscreen.this, get_started.class);
-                        startActivity(intent);
-                    }
-                    finish();
-                }
+        top = AnimationUtils.loadAnimation(this, R.anim.top);
+        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom);
+        imageView.setAnimation(top);
+        textView1.setAnimation(bottom);
+        textView2.setAnimation(bottom);
 
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splashscreen.this, get_started.class);
+                startActivity(intent);
+                finish();
             }
-        };thread.start();
+        },SPLASH_SCREEN);
+
     }
 }
